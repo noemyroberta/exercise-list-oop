c2f25d16936847f9b5cfc6e79332efbab2e4cacf
Noemy Roberta <noemy.oliveira2@outlook.com>
2022-09-26 21:16:28 -0300

    refactor: code reformat on `exercise_4`

--- lib/exercise_4/main.dart
+++ lib/exercise_4/main.dart
@@ -3,18 +3,22 @@ import 'rectangle.dart';
 import 'shape.dart';
 
 void main() {
+  List<Shape> shapes = <Shape>[];
 
   print('**Shape #1 data:**');
   ShapeColor rectColor = ShapeColor.BLACK;
   Rectangle rect = Rectangle(width: 4.0, height: 5.0, shapeColor: rectColor);
   print(rect);
 
+
   print('\n**Shape #2 data:**');
   ShapeColor circleColor = ShapeColor.RED;
   Circle circle = Circle(radius: 3.0, shapeColor: circleColor);
   print(circle);
 
+  shapes.add(rect);
+  shapes.add(circle);
+
   print('\nSHAPE AREAS:');
-  print(rect.area());
-  print(circle.area());
+  for(Shape shape in shapes) print(shape.area());
 }
